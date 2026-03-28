8b9fd2a4b2d54b1c9495887b435dbe3c579a4f9b
ahmadqamhyieh <ahmadqamhyieh00@gmail.com>
2026-03-28 14:34:29 +0300

    just testing

---
+++ Example.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.example;
+
+/**
+ *
+ * @author Ahmad
+ */
+public class Example {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("hi its me");
+    }
+}
